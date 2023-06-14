b4bd6a4cec5e9ae1fc7fea2d97b29a6312f53bd2
yoko0180 <yoko0180@gmail.com>
2023-06-14 18:58:13 +0900

    make json

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+const fs = require("fs")
 console.log("hello init !!!!!");
+
+const json = JSON.stringify({
+  name: "foo"
+}, null, 2)
+fs.writeFileSync("package.json", json)
